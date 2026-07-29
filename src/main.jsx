f3195f28db9a450bd5aad972d0e7c15b4608fbcb
coderrr-01 <kamleshsahsani@gmail.com>
2026-07-29 12:52:15 +0530

    feat: postMessage route to parent WP page, read ?step on refresh

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import App from "./App";
 import "./assets/styles/style.css";
 import "./assets/styles/media.css";
@@ -11,11 +11,30 @@ import { StepProvider } from "./context/StepContext";
 import { saveToken } from "./services/api";
 
 // Auto-login: read JWT from ?token param (WP iframe) or window.jrnyData
-const _urlToken = new URLSearchParams(window.location.search).get('token');
+const _params = new URLSearchParams(window.location.search);
+const _urlToken = _params.get('token');
 if (_urlToken) saveToken(_urlToken);
 else if (window.jrnyData?.token) saveToken(window.jrnyData.token);
 
+// Read ?step param to restore route on WP page refresh
+const _initialStep = _params.get('step');
+const _initialPath = _initialStep ? '/' + _initialStep : null;
+
+// Notify WP parent of route changes so it can update the browser URL
+function RouteReporter() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    const slug = pathname === '/' ? 'apply' : pathname.replace(/^\//, '');
+    window.parent.postMessage({ type: 'jrny_route', slug }, '*');
+  }, [pathname]);
+  return null;
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode><BrowserRouter><StepProvider><App /></StepProvider></BrowserRouter></React.StrictMode>,
+  <React.StrictMode>
+    <BrowserRouter initialEntries={_initialPath ? [_initialPath] : undefined}>
+      <StepProvider><RouteReporter /><App /></StepProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
 
