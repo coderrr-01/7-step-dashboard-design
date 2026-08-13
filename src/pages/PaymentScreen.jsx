ed22fb809efbdfeb9f8c8ec962f208b024581666
coderrr-01 <kamleshsahsani@gmail.com>
2026-08-13 12:08:09 +0530

    dynamic value test2

--- src/pages/PaymentScreen.jsx
+++ src/pages/PaymentScreen.jsx
@@ -449,8 +449,8 @@ export default function PaymentScreen() {
                               <span className="fw-medium">{depositAmount}</span>
                            </div>
                            <div className="d-flex justify-content-between small mb-4">
-                              <span className="text-muted">Administrative Fee</span>
-                              <span className="fw-medium">$ 0.00</span>
+                              <span className="text-muted">Rent Amount</span>
+                              <span className="fw-medium">${rentAmount}</span>
                            </div>
                            <hr className="my-4 opacity-10" />
                            <div className="d-flex justify-content-between align-items-baseline pt-2">
